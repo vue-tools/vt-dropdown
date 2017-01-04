78d7c069adca01a69cd3d57d255385d6a93ae3fa
2046 <g@ggg.gd>
2017-01-04 18:30:07 +0800

    multiple outputs

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.DropdownItem = exports.DropdownMeu = exports.Dropdown = undefined;
+
+var _menu = require('./menu');
+
+var _menu2 = _interopRequireDefault(_menu);
+
+var _item = require('./item');
+
+var _item2 = _interopRequireDefault(_item);
+
+var _index = require('./index.vue');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Dropdown = _index2.default;
+exports.DropdownMeu = _menu2.default;
+exports.DropdownItem = _item2.default;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import DropdownMeu from './menu'
+import DropdownItem from './item'
+import Dropdown from './index.vue'
+
+export {
+    Dropdown,
+    DropdownMeu,
+    DropdownItem
+}
